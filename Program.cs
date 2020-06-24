7ab578893f127a0b1176ad38dc6367cc1bdf6d99
buzzrabone <43200129+buzzrabone@users.noreply.github.com>
2020-06-24 13:22:54 +0100

    Quick update
    added s to hello worlds

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helllooooo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlds!");
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
